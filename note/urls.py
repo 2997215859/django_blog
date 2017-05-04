b459f40baf442c055a5be031cd74d9ca7d719261
DESKTOP-O6VHCR8\ruiy~~ <2997215859@qq.com>
2017-05-04 23:09:16 +0800

    添加刷新当前页显示正确内容的url

--- note/urls.py
+++ note/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url, include
 from . import views
 urlpatterns = [
     url(r'^index/$', views.index),
-    url(r'^album/$', views.album)
+    url(r'^album/$', views.album),
+    url(r'^(?P<note_id>(\w|\d)+)$', views.note_detail)
 ]
 # getNoteContent?noteId = 5848
 # ecbf26384336a4000001
--- note/views.py
+++ note/views.py
@@ -1,9 +1,21 @@
+# coding=utf-8
 from django.shortcuts import render, render_to_response
-from django.http import HttpResponseRedirect
 from django.template import RequestContext
+from api_note.models import Note
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 
 def index(request):
     return render(request, 'note/index3.html')
 
 def album(request):
     return render(request, 'note/album.html')
+
+def note_detail(request, note_id):
+    try:
+        note = Note.objects.get(NoteId=note_id)
+    except Note.DoesNotExist:
+        return Response({'status': 'this note does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+    cur_notebook_id = note.NotebookId
+    return render(request, 'note/index3.html', {"cur_id": note_id, "cur_notebook_id": cur_notebook_id})
